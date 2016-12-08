d3c3d9e63bcb2cf70b5d8467b8cc305c74e7391b
Monsieur Cellophane <MrC3llophane@gmail.com>
2016-12-08 18:45:52 +0100

    Read secret from settings; some test code in bauth/compat.py (run python bauth/compat.py)

--- bauth/compat.py
+++ bauth/compat.py
@@ -1,3 +1,12 @@
+if __name__=="__main__":
+    import os
+    import sys
+    from os.path import dirname, abspath
+    #sys.path.insert(0,dirname(dirname(abspath(__file__))))
+    sys.path.append(dirname(dirname(abspath(__file__))))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reddit.settings")
+
+
 from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
@@ -54,19 +63,23 @@ def session_key(N=16):
 def encode_secret():
     """Return a systemwide fixed secret"""
     #TODO: Read from settings
-    return "Trullallero lero la"
+    try:
+        return settings.ITINF_SECRET
+    except AttributeError:
+        msg="Define ITINF_SECRET in settings.py"
+        raise AttributeError(msg)
 
 def encoded_payload(username):
     """Return encoded payload"""
     pl={'username':username,'time':str(time.time()),'sessionkey':session_key()}
     # or use base64
-    return (simplejson.dumps(pl)).encode('hex')
+    return (json.dumps(pl)).encode('hex')
 
 def decoded_payload(pl):
     """Return decoded payload"""
     # or use base64
     try:
-        return (simplejson.loads(pl.decode('hex')))
+        return (json.loads(pl.decode('hex')))
     except ValueError:
         return None
     except TypeError:
@@ -90,3 +103,10 @@ def verify_handler(token):
     if mac != encode_hmac(pl): return None
     return decoded_payload(pl)
 
+if __name__=="__main__":
+    import os
+
+    token=encode_handler("You can see right through me")
+    sys.stderr.write("Token:%s\n"%token)
+    sys.stderr.write("Decoded:%s\n"%repr(verify_handler(token)))
+    
