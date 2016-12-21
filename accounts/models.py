a306cd7a0179e1a33879a25b015671387089fd3a
Monsieur Cellophane <MrC3llophane@gmail.com>
2016-12-21 11:26:04 +0100

    Factored out receivers in own file, renamed user field to owner

--- accounts/models.py
+++ accounts/models.py
@@ -6,22 +6,16 @@ from django.contrib.auth.models import User
 # This needs to be added to settings.py as:
 # AUTH_PROFILE_MODULE = 'accounts.Profile'
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    """
+    Holds user details, preferences etc.
+    """
+    # See https://docs.djangoproject.com/en/1.10/ref/models/fields/
+    # for other on_delete possible values.
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     is_confirmed=models.BooleanField(null=False, blank=False, default=False)
+    is_deleted=models.BooleanField(null=False, blank=False, default=False)
 
-#https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html    
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
     
