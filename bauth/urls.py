48f81d5646fc649e9200a91e08cf34b0f17eedac
Monsieur Cellophane <MrC3llophane@gmail.com>
2016-12-22 10:37:21 +0100

    Auth gets a browsable root

--- bauth/urls.py
+++ bauth/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from views import obtain_token, verify_token
+from views import obtain_token, verify_token, api_root
 
 urlpatterns = [
-    url(r'^token/$', obtain_token),
-    url(r'^verify(/(?P<token>[a-fA_F0-9]+,[a-fA_F0-9]+))?/$', verify_token),
+    url(r'^$', api_root,name='api-root'),
+    url(r'^token/$', obtain_token,name='obtain-token'),
+    url(r'^verify(/(?P<token>[a-fA_F0-9]+,[a-fA_F0-9]+))?/$', verify_token,name='verify-token'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
