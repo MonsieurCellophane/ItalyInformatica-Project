273e27f0f699670e5993e939fe33ae9864e5dc32
Monsieur Cellophane <MrC3llophane@gmail.com>
2016-12-22 09:42:39 +0100

    Schema urls factored out

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
+
 from rest_framework import routers
 
 from accounts.views import ProfileViewSet,UserViewSet,GroupViewSet
-from accounts.views import schema_view, openapi_view, swagger_view
 
 
 
@@ -12,9 +12,8 @@ router.register(r'users'   , UserViewSet)
 router.register(r'groups'  , GroupViewSet)
 
 # Wire up our API using automatic URL routing.
+# NB: DO NOT use format_suffix_patterns here: routers do that, already
 urlpatterns = [
-    url('^schema/$', schema_view),
-    url('^swagger/$', openapi_view),
-    url('^swagger_view/$', swagger_view),
     url(r'^', include(router.urls)),
 ]
+
