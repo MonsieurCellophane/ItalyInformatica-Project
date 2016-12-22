3a78171e3a09aa54c5bb10b74b5ef696b8c6c1e2
Monsieur Cellophane <MrC3llophane@gmail.com>
2016-12-22 09:49:23 +0100

    Removing schema vestiges

--- bauth/views.py
+++ bauth/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from serializers import TokenSerializer, VerifyTokenSerializer
-from renderers import SwaggerRenderer
 from utils import verify_handler
 
 class TokenAPIView(APIView):
