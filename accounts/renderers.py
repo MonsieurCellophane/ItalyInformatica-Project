7e1e7bd8f39769550625bf596842ccdf3e88bcc1
Monsieur Cellophane <MrC3llophane@gmail.com>
2016-12-22 09:47:45 +0100

    Now empty

--- accounts/renderers.py
+++ accounts/renderers.py
@@ -1,14 +1,1 @@
-from rest_framework import renderers
-from openapi_codec import OpenAPICodec
-
-class SwaggerRenderer(renderers.BaseRenderer):
-    """
-    Simple implementation of the swagger renderer for our apis.
-    Straight from the rest framework tutorial.
-    """
-    media_type = 'application/openapi+json'
-    format = 'swagger'
-
-    def render(self, data, media_type=None, renderer_context=None):
-        codec = OpenAPICodec()
-        return codec.dump(data)
+# renderers, anyone?
