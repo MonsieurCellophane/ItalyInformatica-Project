46da872d38436e9bb57f02e5d21c6c0571efd472
Monsieur Cellophane <MrC3llophane@gmail.com>
2016-12-23 17:49:33 +0100

    Fixed title.

--- root/views.py
+++ root/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     context={
-        'description':"Hello, world. You're at the ItalyInformatica project index.",
+        #'description':"Hello, world. You're at the ItalyInformatica project index.",
+        'name':"The Italy Informatica Project - Backend",
         }
     return render(request,'root/index.html', context)
 
