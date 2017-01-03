fe1895e0ab3df66437d0a28b339bf1d13be5862a
Monsieur Cellophane <MrC3llophane@gmail.com>
2017-01-03 18:15:46 +0100

    Connected receivers

--- accounts/apps.py
+++ accounts/apps.py
@@ -6,3 +6,6 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     name = 'accounts'
     verbose_name="Accounts for the /r/italyinformatica project"
+    def ready(self):
+        #import ipdb; ipdb.set_trace();
+        import receivers
