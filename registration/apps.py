1a7e1640eca164f5bc62d1900013817da7e8a55f
Monsieur Cellophane <MrC3llophane@gmail.com>
2017-01-03 18:16:30 +0100

    Connected receivers

--- registration/apps.py
+++ registration/apps.py
@@ -5,3 +5,8 @@ from django.apps import AppConfig
 
 class RegistrationConfig(AppConfig):
     name = 'registration'
+
+    def ready(self):
+        #import ipdb; ipdb.set_trace();
+        import receivers
+        
