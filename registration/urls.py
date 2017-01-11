70afef3bf06a11657b3ecb75d6799187011c73db
Monsieur Cellophane <MrC3llophane@gmail.com>
2017-01-11 17:54:33 +0100

    changed urls to account for a naming conflict with schema generation (urls cannot be 'list' or 'delete' or...)

--- registration/urls.py
+++ registration/urls.py
@@ -7,9 +7,9 @@ import views
 #ALF: ACHTUNG! look in models.py, since the get_absolute_url method must match this.
 urlpatterns = [
     url(r'^$', views.registration_api_root,name='registration-api-root'),
-    url(r'^list/$',views.RegistrationList.as_view(), name='registration-list'),
-    url(r'^create/$',views.RegistrationCreate.as_view(), name='registration-create'),
-    url('^view/(?P<pk>[0-9]+)/$',views.RegistrationDetail.as_view(), name='registration-detail'),
+    url(r'^all/$',views.RegistrationList.as_view(), name='registration-list'),
+    url(r'^new/$',views.RegistrationCreate.as_view(), name='registration-create'),
+    url('^show/(?P<pk>[0-9]+)/$',views.RegistrationDetail.as_view(), name='registration-detail'),
     url('^verify/$',views.RegistrationVerify.as_view(), name='registration-verify'), #so we can reverse, see receivers.py
     url('^verify/(?P<pk>[0-9]+)/(?P<token>[a-zA-Z0-9,=]+)$',views.RegistrationVerify.as_view(), name='registration-verify'),
 #    url(r'^users/$', views.UserList.as_view(),name="regusers"),
